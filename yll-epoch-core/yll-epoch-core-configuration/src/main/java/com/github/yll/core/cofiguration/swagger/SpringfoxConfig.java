24abddb0b925fb7aaeb3bfdfd8f492213e63a422
luliang_yu <616625797@qq.com>
2018-11-20 17:13:09 +0800

    yll-epock-framework init

---
+++ yll-epoch-core/yll-epoch-core-configuration/src/main/java/com/github/yll/core/cofiguration/swagger/SpringfoxConfig.java
@@ -0,0 +1,49 @@
+package com.github.yll.core.cofiguration.swagger;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Springfox-Swagger2配置
+ *
+ * @author luliang_yu
+ * @date 2018-11-07
+ */
+
+@EnableSwagger2
+@Configuration
+public class SpringfoxConfig {
+
+    //创建Docket Bean
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .ignoredParameterTypes(ApiIgnore.class);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Restful API")
+                .description("API Description")
+                .contact(new Contact("luliang_yu", "http://github.com/arthurknight", ""))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0")
+                .build();
+    }
+}
