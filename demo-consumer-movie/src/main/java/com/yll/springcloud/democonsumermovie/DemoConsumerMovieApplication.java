6c02026708031fff38612324a855101235a6cd18
luliang_yu <616625797@qq.com>
2018-12-10 14:34:21 +0800

    commit

--- demo-consumer-movie/src/main/java/com/yll/springcloud/democonsumermovie/DemoConsumerMovieApplication.java
+++ demo-consumer-movie/src/main/java/com/yll/springcloud/democonsumermovie/DemoConsumerMovieApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableTurbine
 public class DemoConsumerMovieApplication {
 
     @Bean
