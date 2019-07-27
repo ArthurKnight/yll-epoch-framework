8fb923d9580ab84de3a962802f6f1df44299412d
luliang_yu <616625797@qq.com>
2019-07-28 00:17:23 +0800

    feign

---
+++ yll-epoch-core/yll-epoch-core-commons/src/main/java/com/github/yll/epoch/core/commons/Test.java
@@ -0,0 +1,17 @@
+package com.github.yll.epoch.core.commons;
+
+import java.util.HashMap;
+
+/**
+ * @author Arthur
+ * @date 2018-12-15 23:28
+ */
+
+public class Test {
+    public static void main(String[] args) {
+
+        HashMap map = new HashMap(1);
+        map.put(1, 1);
+        map.put(2, 2);
+    }
+}
